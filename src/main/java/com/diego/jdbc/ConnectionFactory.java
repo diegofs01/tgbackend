22baafad758a59ca8736973ed3d3f0582cf9f739
Diego Ferreira <diegofs01@hotmail.com>
2017-11-10 17:13:42 -0200

    Add heroku db

--- src/main/java/com/diego/jdbc/ConnectionFactory.java
+++ src/main/java/com/diego/jdbc/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 	public static Connection getConexao() throws SQLException {
 		try {
 			Class.forName("org.postgresql.Driver");
-			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/tgdb","postgres","postgres");
+			return DriverManager.getConnection("jdbc:postgresql://ec2-50-17-235-5.compute-1.amazonaws.com:5432/d4qhqeo3pvebtc","zyzmyyntejwcrw","2d5807c1501b2ee3d1e06ae700dc4d9f4fd6a5de55df7401e1270ad821ecb45c");
 		} catch (ClassNotFoundException ex) {
 			throw new SQLException(ex.getMessage());
 		}
